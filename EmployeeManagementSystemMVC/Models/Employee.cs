bb75e0f814297b2b64f95307364b960f91770ff9
Md Rafiqul Islam Khan <mdrafiqulislamkhan@hotmail.com>
2019-11-27 17:42:57 +0600

    Employee MOdel Created

---
+++ EmployeeManagementSystemMVC/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeManagementSystemMVC.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string BloodGroup { get; set; }
+        public string Department { get; set; }
+        public string Birthday { get; set; }
+        public int NID { get; set; }
+
+
+    }
+}
